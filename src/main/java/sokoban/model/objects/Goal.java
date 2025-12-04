853498c1584d55c74aa9ee5f4f5b0bef837ac93e
Peter Magdík <petermagdik@gmail.com>
2025-12-04 17:04:11 +0100

    feat: implement Goal

--- src/main/java/sokoban/model/objects/Goal.java
+++ src/main/java/sokoban/model/objects/Goal.java
@@ -1,4 +1,14 @@
 package sokoban.model.objects;
 
-public class Goal {
+import sokoban.model.position.Position;
+
+public class Goal extends StaticObject {
+    public Goal(Position position) {
+        super(position);
+    }
+
+    @Override
+    public boolean isBlocking() {
+        return false;
+    }
 }
