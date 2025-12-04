e6eb6c19a54772d5726c5c13ada094627e45c990
Peter Magdík <petermagdik@gmail.com>
2025-12-04 17:27:55 +0100

    feat: implement MoveableObject

--- src/main/java/sokoban/model/objects/MoveableObject.java
+++ src/main/java/sokoban/model/objects/MoveableObject.java
@@ -1,4 +1,11 @@
 package sokoban.model.objects;
 
-public class MoveableObject {
+import sokoban.model.position.Position;
+
+public abstract class MoveableObject extends GameObject {
+    public MoveableObject(Position position) {
+        super(position);
+    }
+
+    public abstract boolean isBlocking();
 }
