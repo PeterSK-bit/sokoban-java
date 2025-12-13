7bd3f01b918fb510e4d70a16ee7d02d660f54ad4
Peter Magdík <petermagdik@gmail.com>
2025-12-13 02:04:09 +0100

    feat: implemented abstract UIElement

--- src/main/java/sokoban/ui/UIElement.java
+++ src/main/java/sokoban/ui/UIElement.java
@@ -1,4 +1,31 @@
 package sokoban.ui;
 
-public class UIElement {
+import sokoban.model.position.Position;
+
+public abstract class UIElement {
+    private Position position;
+    private boolean visible;
+
+    public UIElement(Position position, boolean visible) {
+        this.position = position;
+        this.visible = visible;
+    }
+
+    public void toggleVisibility() {
+        this.visible = !this.visible;
+    }
+
+    public void show() {
+        this.visible = true;
+    }
+
+    public void hide() {
+        this.visible = false;
+    }
+
+    public boolean isClicked(Position mouseClick) {
+        return this.position.equals(mouseClick);
+    }
+
+    public abstract void onClick();
 }
