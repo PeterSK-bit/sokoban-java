66b809d39adbdbbf2faba2ee74a1c54e78babe57
Peter Magdík <petermagdik@gmail.com>
2025-12-19 19:36:38 +0100

    feat: implement RenderNode

--- src/main/java/sokoban/render/RenderNode.java
+++ src/main/java/sokoban/render/RenderNode.java
@@ -1,4 +1,38 @@
 package sokoban.render;
 
+import sokoban.model.position.Position;
+import sokoban.render.enums.RenderType;
+
+import java.util.List;
+
 public class RenderNode {
+    private Position position;
+    private RenderType type;
+    private List<IVisualNode> shape;
+
+    public RenderNode(Position position, List<IVisualNode> shape, RenderType type) {
+        this.position = position;
+        this.shape = shape;
+        this.type = type;
+    }
+
+    public void updatePosition(Position newPosition) {
+        if (newPosition == null) {
+            throw new IllegalArgumentException("New position can not by null");
+        }
+
+        this.position = newPosition;
+    }
+
+    public void show() {
+        for (IVisualNode v : this.shape) {
+            v.show();
+        }
+    }
+
+    public void hide() {
+        for (IVisualNode v : this.shape) {
+            v.hide();
+        }
+    }
 }
