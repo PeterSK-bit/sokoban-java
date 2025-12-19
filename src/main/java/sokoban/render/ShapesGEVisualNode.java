2bc99137fce78e5638da7cab962ee40ed2b8e22c
Peter Magdík <petermagdik@gmail.com>
2025-12-19 18:40:49 +0100

    feat: implement ShapesGEVisualNode without setPosition and remove

---
+++ src/main/java/sokoban/render/ShapesGEVisualNode.java
@@ -0,0 +1,35 @@
+package sokoban.render;
+
+import sokoban.model.position.Position;
+
+public class ShapesGEVisualNode implements IVisualNode {
+
+    private final Object shape;
+    private final VisualWrapper visibility;
+
+    public ShapesGEVisualNode(Object shape) {
+        this.shape = shape;
+        this.visibility = new VisualWrapper(shape);
+    }
+
+    @Override
+    public void show() {
+        this.visibility.makeVisible();
+    }
+
+    @Override
+    public void hide() {
+        this.visibility.makeInvisible();
+    }
+
+    @Override
+    public void setPosition(Position p) {
+        // no need for it yet
+    }
+
+    @Override
+    public void remove() {
+        // no need for it yet
+    }
+}
+
