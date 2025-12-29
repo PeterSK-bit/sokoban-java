287a72d24de9363547f5dddf8fc49f1cd27b797a
Peter Magdík <petermagdik@gmail.com>
2025-12-29 20:28:45 +0100

    feat: implement skeleton for GameController

--- src/main/java/sokoban/controller/GameController.java
+++ src/main/java/sokoban/controller/GameController.java
@@ -1,4 +1,32 @@
 package sokoban.controller;
 
+import sokoban.input.InputAdapter;
+import sokoban.model.level.Level;
+import sokoban.movement.GameState;
+import sokoban.movement.MovementManager;
+import sokoban.persistence.LevelRepository;
+import sokoban.render.core.Renderer;
+import sokoban.ui.UIController;
+import sokoban.ui.enums.UIState;
+
 public class GameController {
+    private Level currentLevel;
+    private GameState gameState;
+    private final Renderer renderer;
+    private final InputAdapter inputAdapter;
+    private final UIController uiController;
+    private final LevelRepository levelRepository;
+    private final MovementManager movementManager;
+    private UIState uiState = UIState.MAIN_MENU;
+
+    public GameController(
+            Renderer renderer, UIController uiController, InputAdapter inputAdapter,
+            LevelRepository levelRepository, MovementManager movementManager
+    ) {
+        this.renderer = renderer;
+        this.uiController = uiController;
+        this.inputAdapter = inputAdapter;
+        this.levelRepository = levelRepository;
+        this.movementManager = movementManager;
+    }
 }
