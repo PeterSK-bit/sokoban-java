49d3c4454a3e97192c25f520711adcfd59227396
Peter Magdík <petermagdik@gmail.com>
2025-12-29 19:46:43 +0100

    feat: implement Renderer

--- src/main/java/sokoban/render/core/Renderer.java
+++ src/main/java/sokoban/render/core/Renderer.java
@@ -1,4 +1,11 @@
 package sokoban.render.core;
 
+import java.util.List;
+
 public class Renderer {
+    public void render(List<RenderNode> scene) {
+        for (RenderNode node : scene) {
+            node.show();
+        }
+    }
 }
