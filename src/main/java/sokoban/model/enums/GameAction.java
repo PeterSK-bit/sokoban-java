28dc4f7f752ab5d90bbf41b16efefbb1985e04f4
Peter Magdík <petermagdik@gmail.com>
2026-01-03 12:08:14 +0100

    feat: implement GameAction enum

--- src/main/java/sokoban/model/enums/GameAction.java
+++ src/main/java/sokoban/model/enums/GameAction.java
@@ -1,4 +1,13 @@
 package sokoban.model.enums;
 
-public class GameAction {
+public enum GameAction {
+    MOVE_UP,
+    MOVE_DOWN,
+    MOVE_RIGHT,
+    MOVE_LEFT,
+    SAVE_GAME,
+    LOAD_GAME,
+    OPEN_MAIN_MENU,
+    CONFIRM,
+    CANCEL;
 }
